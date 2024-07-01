af6b0259126cabf6665f839b0d4889e6da4dbb48
XXenoss <ahasunatique@gmail.com>
2024-07-01 16:49:49 +0600

    button added

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-var kit_number = document.querySelectorAll(".kit-button").length;
+document.addEventListener("DOMContentLoaded", function() {
+    var kitButtons = document.querySelectorAll(".kit-button");
 
-for (var i=0; i<kit_number; i++){
-    document.querySelectorAll(".kit-button")[i].addEventListener("click", function(){
-        var text_in_kit = this.innerHTML
+    kitButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            var textInKit = this.innerHTML;
+            console.log("Button clicked: " + textInKit);
+            makeSound(textInKit);
+        });
+    });
 
-        makeSound(text_in_kit)
+    document.addEventListener("keypress", function(event) {
+        console.log("Key pressed: " + event.key);
+        makeSound(event.key.toUpperCase());
+    });
 
-
-    })
-}
-
-document.addEventListener("keypress", function(event){
-    makeSound(event.key)
-})
-
-function makeSound(key){
-    switch (key){
-        case "E":
-            var sound1 = new Audio('sounds/e1.mp3');
-            sound1.play();
-            break;
-        case "B":
-            var sounde1 = new Audio('sounds/b2.mp3');
-            sounde1.play();
-            break;
-        case "G":
-            var sounde1 = new Audio('sounds/g2.mp3');
-            sounde1.play();
-            break;
-        case "D":
-            var sounde1 = new Audio('sounds/d2.mp3');
-            sounde1.play();
-            break;
-        case "A":
-            var sounde1 = new Audio('sounds/a1.mp3');
-            sounde1.play();
-            break;
-        case "F":
-            var sounde1 = new Audio('sounds/e3.mp3');
-            sounde1.play();
-            break;
-            
+    function makeSound(key) {
+        switch (key) {
+            case "E":
+                var sound1 = new Audio('sounds/e1.mp3');
+                sound1.play();
+                break;
+            case "B":
+                var sound2 = new Audio('sounds/b2.mp3');
+                sound2.play();
+                break;
+            case "G":
+                var sound3 = new Audio('sounds/g2.mp3');
+                sound3.play();
+                break;
+            case "D":
+                var sound4 = new Audio('sounds/d2.mp3');
+                sound4.play();
+                break;
+            case "A":
+                var sound5 = new Audio('sounds/a1.mp3');
+                sound5.play();
+                break;
+            case "F":
+                var sound6 = new Audio('sounds/e3.mp3');
+                sound6.play();
+                break;
+            default:
+                console.log("No sound for key: " + key);
+        }
     }
-
-
-}
+});
